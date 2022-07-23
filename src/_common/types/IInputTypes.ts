97202afb21bc714a3d34e163d8ce5f6da451cb47
Gustavo Santos <73289907+gustavos00@users.noreply.github.com>
2022-07-23 15:09:28 +0100

    Create type interface

---
+++ src/_common/types/IInputTypes.ts
@@ -0,0 +1,25 @@
+export default interface IInputTypes {
+    type:
+        | 'button'
+        | 'checkbox'
+        | 'color'
+        | 'date'
+        | 'datetime-local'
+        | 'email'
+        | 'file'
+        | 'hidden'
+        | 'image'
+        | 'month'
+        | 'number'
+        | 'password'
+        | 'radio'
+        | 'range'
+        | 'reset'
+        | 'search'
+        | 'submit'
+        | 'tel'
+        | 'text'
+        | 'time'
+        | 'url'
+        | 'week';
+}
