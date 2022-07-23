f4ec858a979e97b5f8c8e8c59e9062adf2626c75
Gustavo Santos <73289907+gustavos00@users.noreply.github.com>
2022-07-23 15:08:27 +0100

    Create Login and Register slider

---
+++ src/components/LoginAndRegisterSlider/index.tsx
@@ -0,0 +1,14 @@
+import * as S from './styles'
+
+interface ILoginAndRegisterSlider {
+  animation: string
+}
+
+export function LoginAndRegisterSlider() {
+    return (
+      <S.Container>
+        <h1>Teste</h1>
+      </S.Container>
+    );
+  }
+  
---
+++ src/components/LoginAndRegisterSlider/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 50vw;
+  height: 100vh;
+  border-top-right-radius: 40px;
+  border-bottom-right-radius: 40px;
+  background-color: ${props => props.theme.colors.bg_color_dark};
+`;
